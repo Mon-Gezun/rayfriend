8d64e20a011b853f991626b6ab828f7125dc1c95
Raymond <raymond@philippinedev.com>
2021-07-18 13:14:19 +0800

    show pending status

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,10 @@ module UsersHelper
   def relationship(user)
     if user == current_user
       "self" 
-    elsif current_user.friend? user
+    elsif current_user.friend?(user)
       "friend" 
+    elsif current_user.pending_friend_request?(user)
+      "pending"
     else
       "?"
     end
